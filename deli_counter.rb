c312f85ce726d7a4b711b48bb44844ce9f440d1e
Berkley Brown <berkleydeane@gmail.com>
2020-05-29 19:17:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,6 +6,7 @@ def line(array)
   new_modified_array = array.map do |name| 
     
     end
+    
     puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
   puts "The line is currently empty."
 end
