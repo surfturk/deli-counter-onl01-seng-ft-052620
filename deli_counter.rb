8dc3bc067619f53dc377de3f5c6dd956983522d3
Berkley Brown <berkleydeane@gmail.com>
2020-05-29 19:24:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(array)
     end
     
     if array.length > 0
-    puts "The line is currently: #{new_modified_array.join(" ")}"
+    puts "The line is currently: #{array.join(" ")}"
   else
   puts "The line is currently empty."
 end
