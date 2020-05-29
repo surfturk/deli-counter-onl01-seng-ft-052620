33b463303183dfbdc99895b318411ca07353e11c
Berkley Brown <berkleydeane@gmail.com>
2020-05-29 19:17:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,13 @@
 def line(array)
   
   
-  puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
-  puts "The line is currently empty."
+ 
   
-  new_modified_array = array.map do |name| end
+  new_modified_array = array.map do |name| 
     
+    end
+    puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
+  puts "The line is currently empty."
 end
 
   
