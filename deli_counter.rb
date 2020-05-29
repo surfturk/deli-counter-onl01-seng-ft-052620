65154cafed86d68531e5c0bcabb0f4c9683f8bcf
Berkley Brown <berkleydeane@gmail.com>
2020-05-29 19:00:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def line(array)
+  if array.empty?
+   puts "The line is currently empty."
+   else
+    current_line = "The line is currently:"
+    array.each.with_index(1) do |person, i|
+      current_line << " #{i}. #{person}"
+    end
+    puts current_line
+  end
+end
+
+  
+def take_a_number(array, string)
+ new_array = []
+  if array.length == 0
+   array.each.with_index(1) do |string, i|
+   new_array << "Welcome, #{string}. You are number #{i} in line."
+   puts new_array
+ end
+   else array.length >= 1
+    array << "Welcome, #{person}. You are number #{i} in line."
+end  
+
+end
