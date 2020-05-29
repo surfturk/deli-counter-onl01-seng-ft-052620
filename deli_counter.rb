77d822979ddf70cd680fde92a6ab3071400da8fc
Berkley Brown <berkleydeane@gmail.com>
2020-05-29 19:32:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ def take_a_number(array, name)
 end
 
 def now_serving(array)
-  
+  if array.length > 0
   puts "Currently serving #{array.shift}."
   puts "There is nobody waiting to be served!"
 end  
