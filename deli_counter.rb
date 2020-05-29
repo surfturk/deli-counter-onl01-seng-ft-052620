b5de16d15ffeb84064ca3fa4bf7aca99e26e8641
Berkley Brown <berkleydeane@gmail.com>
2020-05-29 19:35:21 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def line(array)
+   array.map!.with_index(1) do |name, index| 
+      "#{index}. #{name}"
+    end
+    
+    if array.length > 0
+    puts "The line is currently: #{array.join(" ")}"
+  else
+  puts "The line is currently empty."
+end
+end
+
+  
+def take_a_number(array, name)
+ array << name
+ puts "Welcome, #{name}. You are number #{array.length} in line."
+end
+
+
+def now_serving(array)
+  if array.length > 0
+  puts "Currently serving #{array.shift}."
+  else
+  puts "There is nobody waiting to be served!"
+end  
+end
